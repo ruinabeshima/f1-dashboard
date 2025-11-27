04ea7417c8e7530539f88938b980e7716edd69a0
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-11-27 12:34:22 +0900

    Intialised project

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 
 const PORT = process.env.SERVER_PORT || 5000; 
 const app = express() 
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send("Hello World!")
